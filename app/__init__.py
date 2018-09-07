f0f0ec60352834e5efd68296c160a588f7dfb49c
damunza <danidamunza@gmail.com>
2018-09-07 14:47:42 +0300

    was able to display the registration form

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config_options
 
 bootstrap = Bootstrap()# remember the brackets
 db = SQLAlchemy()
+    # initializing login manager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
 
@@ -17,9 +23,14 @@ def create_app(config_name):
     #where flask extensions are initialized
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # to link the auth folder
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from . import db
+from flask_login import UserMixin
+from . import db, login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Pitch(db.Model):
 
@@ -7,6 +9,7 @@ class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     category = db.Column(db.String)
     pitch = db.Column(db.String)
+    author = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def save_pitch(self):
         db.session.add(self)
@@ -17,12 +20,32 @@ class Pitch(db.Model):
         pitch = Pitch.query.filter_by(category=cat).all()
         return pitch
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    pass_secure = db.Column(db.String(255))  # to store passwords
+    pitch = db.relationship('Pitch', backref = 'user', lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+
 
     def __repr__(self):
         return f'User {self.username}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
