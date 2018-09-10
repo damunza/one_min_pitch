eb90272528aacb4b9926f4f35e6c0864031a159f
damunza <danidamunza@gmail.com>
2018-09-10 11:55:47 +0300

    imported missing modules

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_login import login_required
 from . import main
 from .forms import PitchForm, CommentForm
