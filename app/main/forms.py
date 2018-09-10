ddfd3870eea7dfc7bc02e558edc44a0d11c7d09b
damunza <danidamunza@gmail.com>
2018-09-10 11:27:52 +0300

    added a dropddown for the cartegories

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
-    category = StringField('Category',validators=[Required()])
+    category = SelectField(u'Pitch Category', choices=[('business', 'business'), ('pick-up', 'pick-up'), ('interview', 'interview')])
     pitch = TextAreaField('Your Pitch')
     submit = SubmitField('Submit')
 
